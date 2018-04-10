9042328a0e57b586afb3d8c54ea09c147e6af319
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-04-10 16:15:23 -0400

    ITS A CUBEgit add .! just changed the rendering to display a cube instead of a flat image.

--- src/com/jeffwhenderson/engineTestDrive/MainGameLoop.java
+++ src/com/jeffwhenderson/engineTestDrive/MainGameLoop.java
@@ -24,35 +24,94 @@ public class MainGameLoop {
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
 		
-		float[] vertices = { /// <-------------------------------- define triangles counter clockwise by default 4 OpenGL
-				//left bottom Triangle
-			 -0.5f,  0.5f, 0, // 	VO
-			 -0.5f, -0.5f, 0, //	V1
-			  0.5f, -0.5f, 0, //	V2	
-			  0.5f,  0.5f, 0 //		V3
-		};
-		
-		int[] indices = {
-				0,1,3, // TOP LEFT TRIANGLE -----> VO, V1, V3
-				3,1,2 // BOTTOM RIGHT TRINAGLE --> V3, V1, V2
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
 		};
 		
 		float[] textureCoords = {
-				0,0,	//V0
-				0,1,	//V1
-				1,1,	//V2
-				1,0		//V3
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
 		};
 		
 		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("Coke"));
+		ModelTexture texture = new ModelTexture(loader.loadTexture("facebook-256px"));
 		TexturedModel texturedModel = new TexturedModel(model, texture);
-		Entity entity = new Entity(texturedModel, new Vector3f(0,0,-1),0,0,0,1);
+		Entity entity = new Entity(texturedModel, new Vector3f(0,0,-5),0,0,0,1);
 		Camera camera = new Camera();
 		
 		while(!Display.isCloseRequested()) {
 			// game logic
-			entity.increasePosition(0, 0, -0.0f); //<----------- this should push the image back into the screen as if it were farther away.
+			entity.increaseRotation(1, 1, 1);;
 			camera.move();
 			renderer.prepare();
 			shader.start();
