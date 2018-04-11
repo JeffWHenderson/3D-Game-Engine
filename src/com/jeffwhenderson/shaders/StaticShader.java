2b9d8aeab8592145184a78717dac59db442c6215
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-04-11 19:29:56 -0400

    set up shaders for the Lights. TODO is add normals to the VAOs.

--- src/com/jeffwhenderson/shaders/StaticShader.java
+++ src/com/jeffwhenderson/shaders/StaticShader.java
@@ -2,6 +2,7 @@ package com.jeffwhenderson.shaders;
 
 import org.lwjgl.util.vector.Matrix4f;
 
+import com.jeffwhenderson.engineTestDrive.Light;
 import com.jeffwhenderson.entities.Camera;
 import com.jeffwhenderson.toolbox.Maths;
 
@@ -12,6 +13,8 @@ public class StaticShader extends ShaderProgram {
 	private int location_transformationMatrix;
 	private int location_projectionMatrix;
 	private int location_viewMatrix;
+	private int location_lightPosition;
+	private int location_lightColor;
 	
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -28,10 +31,17 @@ public class StaticShader extends ShaderProgram {
 		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
 		location_projectionMatrix = super.getUniformLocation("projectionMatrix");
 		location_viewMatrix = super.getUniformLocation("viewMatrix");
+		location_lightPosition = super.getUniformLocation("lightPosition");
+		location_lightColor = super.getUniformLocation("lightColor");
 	}
 	
 	public void loadTransformationMatrix(Matrix4f matrix) {
 		super.loadMatrix(location_transformationMatrix, matrix);
+	}
+	
+	public void loadLight(Light light) {
+		super.loadVector(location_lightPosition, light.getPosition());
+		super.loadVector(location_lightColor, light.getColor());
 	}
 	
 	public void loadViewMatrix(Camera camera) {
